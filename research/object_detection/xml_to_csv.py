54477acc3544f3925d3eca53ca3e1f4b947fc08c
Jie <jie.zhao.joy@gmail.com>
2019-01-11 12:47:27 +0800

    adjust project structure

--- research/object_detection/xml_to_csv.py
+++ research/object_detection/xml_to_csv.py
@@ -29,8 +29,8 @@ def main():
     for folder in ['train','test']:
         image_path = os.path.join(os.getcwd(), ('images/' + folder))
         xml_df = xml_to_csv(image_path)
-        xml_df.to_csv(('images/' + folder + '_labels.csv'), index=None)
-        print('Successfully converted xml to csv.')
+        xml_df.to_csv(('training_data/' + folder + '_labels.csv'), index=None)
+        print('Successfully converted: ' + folder +' xml to csv.')
 
 
 main()
